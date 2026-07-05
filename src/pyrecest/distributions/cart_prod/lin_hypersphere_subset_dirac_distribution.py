33287d12332b0b7e58d5e259b21fb997d48fedfc
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-05 15:24:17 +0200

    Fix hypersphere subset Dirac marginalization

--- src/pyrecest/distributions/cart_prod/lin_hypersphere_subset_dirac_distribution.py
+++ src/pyrecest/distributions/cart_prod/lin_hypersphere_subset_dirac_distribution.py
@@ -1,8 +1,11 @@
 from typing import Union
 
 # pylint: disable=no-name-in-module,no-member
-from pyrecest.backend import int32, int64
+from pyrecest.backend import asarray, int32, int64
 
+from ..hypersphere_subset.hyperspherical_dirac_distribution import (
+    HypersphericalDiracDistribution,
+)
 from .abstract_lin_hyperhemisphere_cart_prod_distribution import (
     AbstractLinHypersphereSubsetCartProdDistribution,
 )
@@ -16,7 +19,14 @@ class LinHypersphereSubsetCartProdDiracDistribution(
     AbstractLinHypersphereSubsetCartProdDistribution,
 ):
     def __init__(self, bound_dim: Union[int, int32, int64], d, w=None):
+        d = asarray(d)
         AbstractLinHypersphereSubsetCartProdDistribution.__init__(
             self, bound_dim, d.shape[-1] - bound_dim - 1
         )
         LinBoundedCartProdDiracDistribution.__init__(self, d=d, w=w)
+
+    def marginalize_linear(self):
+        return HypersphericalDiracDistribution(
+            self.d[:, : self.bound_dim + 1],  # noqa: E203
+            self.w,
+        )
