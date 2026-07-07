165a5081f34650e477c64b036adff8761e86ab79
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-07 13:12:14 +0200

    Export low-rank hypertoroidal Fourier distribution

---
+++ src/pyrecest/distributions/hypertorus/__init__.py
@@ -0,0 +1,7 @@
+"""Hypertoroidal distribution exports."""
+
+from .low_rank_hypertoroidal_fourier_distribution import (
+    LowRankHypertoroidalFourierDistribution,
+)
+
+__all__ = ("LowRankHypertoroidalFourierDistribution",)
