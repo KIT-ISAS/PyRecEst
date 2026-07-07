bcfebf1ff8b5896113a0733b9befb0d9c607bcb9
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-07 17:44:29 +0200

    Use deterministic dense coefficients in low-rank distribution tests

--- tests/distributions/test_low_rank_hypertoroidal_fourier_distribution.py
+++ tests/distributions/test_low_rank_hypertoroidal_fourier_distribution.py
@@ -4,16 +4,30 @@ import numpy as np
 import numpy.testing as npt
 
 import pyrecest.backend
-from pyrecest.distributions import WrappedNormalDistribution
 from pyrecest.distributions.hypertorus.hypertoroidal_fourier_distribution import (
     HypertoroidalFourierDistribution,
 )
 from pyrecest.distributions.hypertorus.low_rank_hypertoroidal_fourier_distribution import (
     LowRankHypertoroidalFourierDistribution,
 )
-from pyrecest.distributions.hypertorus.toroidal_wrapped_normal_distribution import (
-    ToroidalWrappedNormalDistribution,
-)
+
+
+def _identity_coefficients_1d():
+    coeff = np.zeros(5, dtype=np.complex128)
+    coeff[2] = 1.0 / (2.0 * np.pi)
+    coeff[1] = 0.01 + 0.02j
+    coeff[3] = np.conjugate(coeff[1])
+    return coeff
+
+
+def _identity_coefficients_2d():
+    coeff = np.zeros((3, 3), dtype=np.complex128)
+    coeff[1, 1] = 1.0 / (2.0 * np.pi) ** 2
+    coeff[0, 1] = 0.005 + 0.002j
+    coeff[2, 1] = np.conjugate(coeff[0, 1])
+    coeff[1, 0] = -0.004 + 0.003j
+    coeff[1, 2] = np.conjugate(coeff[1, 0])
+    return coeff
 
 
 @unittest.skipIf(
@@ -29,22 +43,14 @@ class TestLowRankHypertoroidalFourierDistribution(unittest.TestCase):
         )
 
     def test_value_and_pdf_match_dense_1d(self):
-        dense = HypertoroidalFourierDistribution.from_distribution(
-            WrappedNormalDistribution(np.array(1.0), np.array(0.6)),
-            (9,),
-            "identity",
-        )
+        dense = HypertoroidalFourierDistribution(_identity_coefficients_1d(), "identity")
         low_rank = LowRankHypertoroidalFourierDistribution.from_dense(dense)
         xs = np.linspace(0.0, 2.0 * np.pi, 17, endpoint=False)
         npt.assert_allclose(low_rank.value(xs), dense.value(xs), atol=1e-10)
         npt.assert_allclose(low_rank.pdf(xs), dense.pdf(xs), atol=1e-10)
 
     def test_shift_matches_dense_2d(self):
-        dense = HypertoroidalFourierDistribution.from_distribution(
-            ToroidalWrappedNormalDistribution(np.array([1.0, 2.0]), 0.4 * np.eye(2)),
-            (7, 7),
-            "identity",
-        )
+        dense = HypertoroidalFourierDistribution(_identity_coefficients_2d(), "identity")
         low_rank = LowRankHypertoroidalFourierDistribution.from_dense(dense)
         shift = np.array([0.2, -0.5])
         npt.assert_allclose(
@@ -52,16 +58,8 @@ class TestLowRankHypertoroidalFourierDistribution(unittest.TestCase):
         )
 
     def test_predict_additive_noise_matches_dense_2d(self):
-        prior_dense = HypertoroidalFourierDistribution.from_distribution(
-            ToroidalWrappedNormalDistribution(np.array([1.0, 2.0]), 0.4 * np.eye(2)),
-            (7, 7),
-            "identity",
-        )
-        noise_dense = HypertoroidalFourierDistribution.from_distribution(
-            ToroidalWrappedNormalDistribution(np.zeros(2), 0.2 * np.eye(2)),
-            (7, 7),
-            "identity",
-        )
+        prior_dense = HypertoroidalFourierDistribution(_identity_coefficients_2d(), "identity")
+        noise_dense = HypertoroidalFourierDistribution(_identity_coefficients_2d(), "identity")
         predicted_dense = prior_dense.convolve(noise_dense)
         predicted_low_rank = LowRankHypertoroidalFourierDistribution.from_dense(
             prior_dense
@@ -69,15 +67,9 @@ class TestLowRankHypertoroidalFourierDistribution(unittest.TestCase):
         npt.assert_allclose(predicted_low_rank.to_dense(), predicted_dense.coeff_mat, atol=1e-10)
 
     def test_update_multiply_matches_dense_1d(self):
-        prior_dense = HypertoroidalFourierDistribution.from_distribution(
-            WrappedNormalDistribution(np.array(1.0), np.array(0.5)),
-            (9,),
-            "identity",
-        )
-        likelihood_dense = HypertoroidalFourierDistribution.from_distribution(
-            WrappedNormalDistribution(np.array(0.0), np.array(1.0)),
-            (9,),
-            "identity",
+        prior_dense = HypertoroidalFourierDistribution(_identity_coefficients_1d(), "identity")
+        likelihood_dense = HypertoroidalFourierDistribution(
+            _identity_coefficients_1d(), "identity"
         ).shift(np.array([1.5]))
         updated_dense = prior_dense.multiply(likelihood_dense)
         updated_low_rank = LowRankHypertoroidalFourierDistribution.from_dense(
