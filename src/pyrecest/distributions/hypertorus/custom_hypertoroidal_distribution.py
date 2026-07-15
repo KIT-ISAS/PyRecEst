5d6633d1620bbdb212b056a5b82384501c4aed47
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-15 15:47:15 +0200

    Keep custom hypertoroidal PDF normalization import

--- src/pyrecest/distributions/hypertorus/custom_hypertoroidal_distribution.py
+++ src/pyrecest/distributions/hypertorus/custom_hypertoroidal_distribution.py
@@ -1,5 +1,5 @@
 # pylint: disable=no-name-in-module,no-member
-from pyrecest.backend import mod, pi, zeros
+from pyrecest.backend import asarray, mod, pi, zeros
 
 from ..abstract_custom_distribution import AbstractCustomDistribution
 from ..circle.custom_circular_distribution import CustomCircularDistribution
